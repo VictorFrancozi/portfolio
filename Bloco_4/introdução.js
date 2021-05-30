0b72f149d45f33696211cacdee87695fe919c2a1
VictorFrancozi <victor.francozi@gmail.com>
2021-05-29 22:55:00 -0300

    Criando arquivo de exercicios

---
+++ Bloco_4/introdução.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+let test1 = 2;
+let test2 = 4;
+
+if (test2 > 4){
+    console.log('O maior número é: 4');
+} 
+else if (test1 < 4){   
+    console.log('A metade de 4 é 2');
+}
+
+let ifelse = 1;
+
+if (ifelse >= 1){
+    console.log('Positive number');
+}
+else if (ifelse <= -1){
+    console.log('Negative number');
+}
+else {
+    console.log('Number zero');
+}
+
+let anguloA = 60;
+let anguloB = 40;
+let anguloC = 20;
+
+if (anguloA >= 1, anguloB >= 1, anguloC >= 1 ){
+    console.log('É um triângulo válido');
+}
+else {
+    console.log('É um triângulo inválido');
+}   
+    
+ 
+
